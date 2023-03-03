8cc9538ea34ddbeff8d4902f89e3d5435655f0a2
Ariel Davis <amanichai100@gmail.com>
2023-03-02 22:49:48 -0500

    router links

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo incidunt voluptate pariatur eaque assumenda ex repellat alias a? Quidem dolores maxime corporis totam aperiam voluptates quisquam molestias! Cumque voluptates nulla illo esse consectetur doloribus vel neque possimus, a omnis natus repellendus enim perspiciatis excepturi autem temporibus nihil quisquam eum! Culpa officia illo eum delectus iure quae itaque voluptatum sint alias facilis beatae totam, labore cumque, praesentium natus omnis aliquid maiores expedita veniam accusantium suscipit qui. Nihil, quam quisquam impedit vero odit tempore nobis natus fuga molestiae dolore accusamus mollitia ratione cumque facilis omnis tempora error hic itaque recusandae vel vitae.</p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est et corrupti voluptatem ea fugit quia non odio explicabo quidem excepturi, aperiam blanditiis praesentium cum veritatis sunt saepe facere qui accusamus dolores officiis. Molestias dolor a vitae quibusdam magnam earum veniam voluptas dolorum iste consequuntur enim fuga quo mollitia laudantium inventore eius provident eos distinctio eligendi, ut labore similique. Molestias animi quod nihil voluptatem ipsum est dolor delectus deserunt totam? Consequatur provident error earum est ipsa? Magni at voluptates sequi sunt voluptatibus animi nisi illum quis maxime ipsam eveniet alias fugiat consequatur adipisci tempore voluptatem fuga optio, maiores vitae. Reprehenderit, unde!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est et corrupti voluptatem ea fugit quia non odio explicabo quidem excepturi, aperiam blanditiis praesentium cum veritatis sunt saepe facere qui accusamus dolores officiis. Molestias dolor a vitae quibusdam magnam earum veniam voluptas dolorum iste consequuntur enim fuga quo mollitia laudantium inventore eius provident eos distinctio eligendi, ut labore similique. Molestias animi quod nihil voluptatem ipsum est dolor delectus deserunt totam? Consequatur provident error earum est ipsa? Magni at voluptates sequi sunt voluptatibus animi nisi illum quis maxime ipsam eveniet alias fugiat consequatur adipisci tempore voluptatem fuga optio, maiores vitae. Reprehenderit, unde!</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: { }
 }
 </script>
